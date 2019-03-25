9526ef3a5a3b012eab0a229c4e1ec5900778a558
Sparsh5008 <sparsh5008@gmail.com>
2019-03-25 00:57:54 -0700

    buttons added

--- Online_Exam_Sln/Online_Exam/UpcomingTests.h
+++ Online_Exam_Sln/Online_Exam/UpcomingTests.h
@@ -86,103 +86,142 @@ namespace Online_Exam {
 		}
 #pragma endregion
 	private: System::Void UpcomingTests_Load(System::Object^  sender, System::EventArgs^  e) {
-				 contentPanel->Controls->Clear();
-				 int x = 0;
-				 int y = 0;
+				 try
+				 {
 
-				 OES ^ Access = gcnew OES();
-				 Access->ExecQuery("SELECT Groups FROM Users WHERE Username = '" + gVar::Username + "'");
-				 String ^ grp = Convert::ToString(Access->DBDT->Rows[0]["Groups"]);
-				 String ^ query = "";
-				 String ^ temp = "";
-				 for (int i = 0; i < grp->Length-1; i++)
-				 {
-					 if (grp[i] == '-' && grp[i + 1] == '-')
+					 contentPanel->Controls->Clear();
+					 int x = 0;
+					 int y = 0;
+
+					 OES ^ Access = gcnew OES();
+					 Access->ExecQuery("SELECT Groups FROM Users WHERE Username = '" + gVar::Username + "'");
+					 String ^ grp = Convert::ToString(Access->DBDT->Rows[0]["Groups"]);
+					 String ^ query = "";
+					 String ^ temp = "";
+					 for (int i = 0; i < grp->Length - 1; i++)
 					 {
-						 temp += grp[i];
-						 String ^ cur = "GroupID Like '%" + temp + "%' OR ";
-						 query += cur;
-						 temp = "";
+						 if (grp[i] == '-' && grp[i + 1] == '-')
+						 {
+							 temp += grp[i];
+							 String ^ cur = "GroupID Like '%" + temp + "%' OR ";
+							 query += cur;
+							 temp = "";
+						 }
+						 else temp += grp[i];
 					 }
-					 else temp += grp[i];
-				 }
-				 temp = temp + "-";
-				 String ^ cur = "GroupID Like '%" + temp + "%'";
-				 query += cur;
-				 if (grp->Length > 0)
-				 {
-					 Access->ExecQuery("SELECT * FROM Exam WHERE " + query);
-					 //MessageBox::Show("SELECT * FROM Exam WHERE " + query);
-					 for (int i = 0; i < Access->RecordCount; i++)
+					 temp = temp + "-";
+					 String ^ cur = "GroupID Like '%" + temp + "%'";
+					 query += cur;
+					 if (grp->Length > 0)
 					 {
-						 PictureBox^ border = gcnew PictureBox();
-						 Label^ lblExamName = gcnew Label();
-						 Label^ lblProfName = gcnew Label();
-						 Label ^ lblExam = gcnew Label();
-						 Label ^lblProf = gcnew Label();
-						 Button ^bsession1 = gcnew Button();
-						 Button ^bsession2 = gcnew Button();
-						 Button ^bsession3 = gcnew Button();
-						 Button ^bsession4 = gcnew Button();
-						 Button ^bsession5 = gcnew Button();
-						 border->Width = 400;
-						 border->Height = 100;
-						 lblProfName->Width = 100;
-						 lblProfName->Height = 20;
-						 lblProf->Width = 150;
-						 lblProf->Height = 20;
-						 lblExamName->Width = 100;
-						 lblExamName->Height = 20;
-						 lblExam->Width = 150;
-						 lblExam->Height = 20;
-						 lblExam->Text = "Exam Name: ";
-						 lblProf->Text = "Set By: ";
-						 lblExamName->Text = Convert::ToString(Access->DBDT->Rows[i]["ExamName"]);
-						 lblProfName->Text = Convert::ToString(Access->DBDT->Rows[i]["Professor"]);
-						 /*border->BackColor = Color::White;
-						 lblProfName->BackColor = Color::Black;
-						 lblExamName->BackColor = Color::Black;
-						 lblExamName->ForeColor = Color::White;
-						 lblProfName->ForeColor = Color::White;*/
-						 //MessageBox::Show(lblExamName->Text);
-						 //Point ^ pt = gcnew Point(x + 50, y + 5);
-						 //border->Location =Point(x + 50, y + 5);
-						 lblExamName->Location = Point(x +175, y + 30);
-						 lblProfName->Location = Point(x + 175, y + 60);
-						 lblExam->Location = Point(x + 20, y + 30);
-						 lblProf->Location = Point(x + 20, y + 60);
-						 lblExamName->AutoEllipsis = true;
-						 lblProfName->AutoEllipsis = true;
-						 bsession1->AutoEllipsis = true;
-						 bsession1->Text = "Session1";
-						 bsession2->Text = "Session2";
-						 bsession3->Text = "Session3";
-						 bsession4->Text = "Session4";
-						 bsession5->Text = "Session5";
-						 
-						 bsession1->Location = Point(x + 400, y + 10);
-						 bsession2->Location = Point(x + 400, y + 31);
-						 bsession3->Location = Point(x + 400, y + 52);
-						 bsession4->Location = Point(x + 400, y + 73);
-						 bsession5->Location = Point(x + 400, y + 94);
-						 
-						 /*border->SendToBack();*/
-						 contentPanel->Controls->Add(lblExamName);
-						 contentPanel->Controls->Add(lblProfName);
-						 //contentPanel->Controls->Add(border);
-						 contentPanel->Controls->Add(lblExam);
-						 contentPanel->Controls->Add(lblProf);
-						 contentPanel->Controls->Add(bsession1);
-						 contentPanel->Controls->Add(bsession2);
-						 contentPanel->Controls->Add(bsession3);
-						 contentPanel->Controls->Add(bsession4);
-						 contentPanel->Controls->Add(bsession5);
-						 y += 120;
+						 Access->ExecQuery("SELECT * FROM Exam WHERE " + query);
+						 //MessageBox::Show("SELECT * FROM Exam WHERE " + query);
+						 DateTime date = DateTime::Now;
+						 for (int i = 0; i < Access->RecordCount; i++)
+						 {
+
+							 PictureBox^ border = gcnew PictureBox();
+							 Label^ lblExamName = gcnew Label();
+							 Label^ lblProfName = gcnew Label();
+							 Label ^ lblExam = gcnew Label();
+							 Label ^lblProf = gcnew Label();
+							 //creating session buttons
+							 Button ^bsession1 = gcnew Button();
+							 Button ^bsession2 = gcnew Button();
+							 Button ^bsession3 = gcnew Button();
+							 Button ^bsession4 = gcnew Button();
+							 Button ^bsession5 = gcnew Button();
+							 border->Width = 400;
+							 border->Height = 100;
+							 lblProfName->Width = 100;
+							 lblProfName->Height = 20;
+							 lblProf->Width = 150;
+							 lblProf->Height = 20;
+							 lblExamName->Width = 100;
+							 lblExamName->Height = 20;
+							 lblExam->Width = 150;
+							 lblExam->Height = 20;
+							 lblExam->Text = "Exam Name: ";
+							 lblProf->Text = "Set By: ";
+							 lblExamName->Text = Convert::ToString(Access->DBDT->Rows[i]["ExamName"]);
+							 lblProfName->Text = Convert::ToString(Access->DBDT->Rows[i]["Professor"]);
+							 /*border->BackColor = Color::White;
+							 lblProfName->BackColor = Color::Black;
+							 lblExamName->BackColor = Color::Black;
+							 lblExamName->ForeColor = Color::White;
+							 lblProfName->ForeColor = Color::White;*/
+							 //MessageBox::Show(lblExamName->Text);
+							 //Point ^ pt = gcnew Point(x + 50, y + 5);
+							 //border->Location =Point(x + 50, y + 5);
+							 lblExamName->Location = Point(x + 175, y + 30);
+							 lblProfName->Location = Point(x + 175, y + 60);
+							 lblExam->Location = Point(x + 20, y + 30);
+							 lblProf->Location = Point(x + 20, y + 60);
+							 lblExamName->AutoEllipsis = true;
+							 lblProfName->AutoEllipsis = true;
+							 bsession1->AutoEllipsis = true;
+							 bsession1->Text = "Session1";
+							 bsession2->Text = "Session2";
+							 bsession3->Text = "Session3";
+							 bsession4->Text = "Session4";
+							 bsession5->Text = "Session5";
+
+							 bsession1->Location = Point(x + 400, y + 10);
+							 bsession2->Location = Point(x + 400, y + 31);
+							 bsession3->Location = Point(x + 400, y + 52);
+							 bsession4->Location = Point(x + 400, y + 73);
+							 bsession5->Location = Point(x + 400, y + 94);
+
+							 /*border->SendToBack();*/
+							 contentPanel->Controls->Add(lblExamName);
+							 contentPanel->Controls->Add(lblProfName);
+							 //contentPanel->Controls->Add(border);
+							 contentPanel->Controls->Add(lblExam);
+							 contentPanel->Controls->Add(lblProf);
+							 contentPanel->Controls->Add(bsession1);
+							 contentPanel->Controls->Add(bsession2);
+							 contentPanel->Controls->Add(bsession3);
+							 contentPanel->Controls->Add(bsession4);
+							 contentPanel->Controls->Add(bsession5);
+							 y += 120;
+
+							 //getting the dates of all sessions
+							// DateTime ses1Date = Convert::ToDateTime(Access->DBDT->Rows[i]["StartTimeSes1"]);
+							// DateTime ses2Date = Convert::ToDateTime(Access->DBDT->Rows[i]["StartTimeSes2"]);
+							// DateTime ses3Date = Convert::ToDateTime(Access->DBDT->Rows[i]["StartTimeSes3"]);
+							// DateTime ses4Date = Convert::ToDateTime(Access->DBDT->Rows[i]["StartTimeSes4"]);
+							// DateTime ses5Date = Convert::ToDateTime(Access->DBDT->Rows[i]["StartTimeSes5"]);
+//
+							// bsession1->Tag = ses1Date;
+							// bsession2->Tag = ses2Date;
+							// bsession3->Tag = ses3Date;
+							// bsession4->Tag = ses4Date;
+							// bsession5->Tag = ses5Date;
+							 //bsession1->Click += gcnew System::EventHandler(this, &UpcomingTests::btnClick);
+							// bsession2->Click += gcnew System::EventHandler(this, &UpcomingTests::btnClick);
+							// bsession3->Click += gcnew System::EventHandler(this, &UpcomingTests::btnClick);
+							// bsession4->Click += gcnew System::EventHandler(this, &UpcomingTests::btnClick);
+							// bsession5->Click += gcnew System::EventHandler(this, &UpcomingTests::btnClick);
+
+						 }
+						 //MessageBox::Show(query);
+						 //dg1->DataSource = Access->DBDT;
 					 }
-					 //MessageBox::Show(query);
-					 //dg1->DataSource = Access->DBDT;
 				 }
+				 catch (Exception ^ex)
+				 {
+					 MessageBox::Show("error occured");
+				 }
+				
 	}
-	
+	private: System::Void btnClick(System::Object^  sender, System::EventArgs^  e)
+	{
+				 Button ^ btn = gcnew Button();
+				 btn = static_cast<Button^>(sender);
+				 DateTime associatedDate = static_cast<DateTime>(btn->Tag);
+				 String ^format = "yy-MM-dd HH-mm-ss";
+				 String ^newDate = associatedDate.ToString(format);
+				 MessageBox::Show(newDate);
+	}
 };
 }
